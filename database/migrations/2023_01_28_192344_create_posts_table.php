19ab04463913ee16ba16e92786e8dc76f88cd53f
Japheth-Joepari <phython023@gmail.com>
2024-05-25 17:54:23 +0100

    changes made to docker

--- database/migrations/2023_01_28_192344_create_posts_table.php
+++ database/migrations/2023_01_28_192344_create_posts_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->unsignedBigInteger('category_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('name')->unique();
-            $table->longText('description')->unique();
+            $table->string('description')->unique();
             $table->string('image')->nullable();
             $table->timestamps();
         });
