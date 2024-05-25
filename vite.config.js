0b676f73fdf617a9f037a8b7d92a4b3c6b8857d6
Japheth-Joepari <phython023@gmail.com>
2024-05-25 18:22:53 +0100

    removed css

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import laravel, { refreshPaths } from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "public/js",
-                "public/css",
-            ],
+            input: [],
             refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
